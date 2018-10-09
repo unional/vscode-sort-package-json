aec9405b156d899f460258f61bb0f4be0e4d4a15
Homa Wong <homawong@gmail.com>
2018-10-09 13:06:40 -0700

    initial release

---
+++ src/sortPackageJson.ts
@@ -0,0 +1,34 @@
+import sort from 'sort-package-json'
+import vscode, { TextEditor, TextEditorEdit } from 'vscode'
+
+export function sortPackageJson(textEditor: TextEditor, edit: TextEditorEdit) {
+  if (!textEditor.document.fileName.endsWith('package.json')) {
+    vscode.window.showWarningMessage('Active file is not package.json');
+    return
+  }
+
+  const text = textEditor.document.getText()
+  const json = tryParseJson(text)
+
+  if (json) {
+    edit.replace(getWholeTextRange(textEditor), JSON.stringify(sort(json), null, 2))
+  }
+}
+
+function tryParseJson(text) {
+  try {
+    return JSON.parse(text)
+  }
+  catch (e) {
+    return undefined
+  }
+}
+
+function getWholeTextRange(textEditor: TextEditor) {
+  const firstLine = textEditor.document.lineAt(0);
+  const lastLine = textEditor.document.lineAt(textEditor.document.lineCount - 1);
+  return new vscode.Range(0,
+    firstLine.range.start.character,
+    textEditor.document.lineCount - 1,
+    lastLine.range.end.character);
+}
