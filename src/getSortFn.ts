7a63f58eda21aa293c67caad5098a8913228cfc8
Homa Wong <homawong@gmail.com>
2022-04-28 15:47:28 -0700

    fix: update deps

--- src/getSortFn.ts
+++ src/getSortFn.ts
@@ -1,20 +1,20 @@
-import path from 'path';
-import readPkgUp from 'read-pkg-up';
-import resolve from 'resolve';
-import sort from 'sort-package-json';
+import path from 'path'
+import { readPackageUpSync } from 'read-pkg-up'
+import resolve from 'resolve'
+import sort from 'sort-package-json'
 
 export function getSortFn(pjson: any, fileName: string) {
-  const dir = path.dirname(fileName);
+  const dir = path.dirname(fileName)
   if (hasSortPackageJsonPkg(pjson)) {
     const modulePath = resolve.sync('sort-package-json', { basedir: dir })
     return require(modulePath)
   }
 
-  const res = readPkgUp.sync({ cwd: path.resolve(dir, '..'), normalize: false })
+  const res = readPackageUpSync({ cwd: path.resolve(dir, '..'), normalize: false })
 
   if (res?.packageJson) return getSortFn(res.packageJson, res.path)
 
-  return sort;
+  return sort
 }
 
 function hasSortPackageJsonPkg(json: any) {
