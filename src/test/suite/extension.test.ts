d2bc83f01da71519596496f36e9032ac69ea4fbf
Homa Wong <homawong@gmail.com>
2022-04-28 16:12:18 -0700

    chore: update project

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,4 +1,4 @@
-import assert from 'assert';
+import assert from 'assert'
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -10,7 +10,7 @@ suite("Extension Tests", function () {
 
   // Defines a Mocha unit test
   test("Something 1", function () {
-    assert.equal(-1, [1, 2, 3].indexOf(5));
-    assert.equal(-1, [1, 2, 3].indexOf(0));
-  });
-});
+    assert.equal(-1, [1, 2, 3].indexOf(5))
+    assert.equal(-1, [1, 2, 3].indexOf(0))
+  })
+})
