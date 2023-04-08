c34777901eda4d1bb0f30079bad542cd0094bf7b
Karl Horky <karl.horky@gmail.com>
2023-04-08 11:25:57 +0200

    Add code action for sorting on save
    Closes https://github.com/unional/vscode-sort-package-json/issues/2

--- src/extension.ts
+++ src/extension.ts
@@ -4,4 +4,14 @@ import { sortPackageJson } from './sortPackageJson'
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(vscode.commands.registerTextEditorCommand('editor.sortPackageJson', sortPackageJson))
+  context.subscriptions.push(vscode.languages.registerCodeActionsProvider({ language: 'json', pattern: '**/package.json' }, new CodeActionProvider()))
+}
+
+export class CodeActionProvider implements vscode.CodeActionProvider {
+  public provideCodeActions(): ProviderResult<vscode.Command[]> {
+    return [{
+      command: 'editor.sortPackageJson',
+      title: 'Sort well-known keys in package.json',
+    }]
+  }
 }
