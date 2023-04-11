d83e386bed08418d4c95fb948d0af80b2c71949e
Homa Wong <homawong@gmail.com>
2023-04-11 02:36:50 -0700

    Update project (#69)
    * chore: update project
    
    * chore: remove tslint config
    
    * chore: add GH Action
    
    * chore: compile -> build
    
    * chore: try CI example from gh action
    
    * chore: add pnpm
    
    * chore: remove test
    
    will add jest
    
    * chore: restore shared CI config

--- src/getSortFn.ts
+++ src/getSortFn.ts
@@ -1,23 +1,25 @@
-import path from 'path'
+import * as path from 'path'
 import { readPackageUpSync } from 'read-pkg-up'
-import resolve from 'resolve'
-import sortPackageJsonExports from 'sort-package-json'
+import * as resolve from 'resolve'
+import * as sortPackageJsonExports from 'sort-package-json'
 
 export function getSortFn(pjson: any, fileName: string): sortPackageJsonExports {
-  const dir = path.dirname(fileName)
-  if (hasSortPackageJsonPkg(pjson)) {
-    const modulePath = resolve.sync('sort-package-json', { basedir: dir })
-    return require(modulePath)
-  }
+	const dir = path.dirname(fileName)
+	if (hasSortPackageJsonPkg(pjson)) {
+		const modulePath = resolve.sync('sort-package-json', { basedir: dir })
+		return require(modulePath)
+	}
 
-  const res = readPackageUpSync({ cwd: path.resolve(dir, '..'), normalize: false })
+	const res = readPackageUpSync({ cwd: path.resolve(dir, '..'), normalize: false })
 
-  if (res?.packageJson) return getSortFn(res.packageJson, res.path)
+	if (res?.packageJson) return getSortFn(res.packageJson, res.path)
 
-  return sortPackageJsonExports
+	return sortPackageJsonExports
 }
 
 function hasSortPackageJsonPkg(json: any) {
-  return json.dependencies && json.dependencies['sort-package-json'] ||
-    json.devDependencies && json.devDependencies['sort-package-json']
+	return (
+		(json.dependencies && json.dependencies['sort-package-json']) ||
+		(json.devDependencies && json.devDependencies['sort-package-json'])
+	)
 }
--- src/sortPackageJson.ts
+++ src/sortPackageJson.ts
@@ -1,41 +1,42 @@
-import vscode, { TextEditor, TextEditorEdit } from 'vscode'
+import { Range, TextEditor, TextEditorEdit, window } from 'vscode'
 import { getSortFn } from './getSortFn'
 
 export function sortPackageJson(textEditor: TextEditor, edit: TextEditorEdit) {
-  if (!textEditor.document.fileName.endsWith('package.json')) {
-    vscode.window.showWarningMessage('Active file is not package.json')
-    return
-  }
+	if (!textEditor.document.fileName.endsWith('package.json')) {
+		window.showWarningMessage('Active file is not package.json')
+		return
+	}
 
-  const text = textEditor.document.getText()
-  const json = tryParseJson(text)
+	const text = textEditor.document.getText()
+	const json = tryParseJson(text)
 
-  if (json) {
-    const sort = getSortFn(json, textEditor.document.fileName)
-    const text = getWholeTextRange(textEditor)
-    edit.replace(text, JSON.stringify(sort(json), null, 2))
-    // insert newline if previously present
-    if (text.end.character === 0) {
-      edit.insert(text.end, '\n')
-    }
-  }
+	if (json) {
+		const sort = getSortFn(json, textEditor.document.fileName)
+		const text = getWholeTextRange(textEditor)
+		edit.replace(text, JSON.stringify(sort(json), null, 2))
+		// insert newline if previously present
+		if (text.end.character === 0) {
+			edit.insert(text.end, '\n')
+		}
+	}
 }
 
 function tryParseJson(text: string) {
-  try {
-    return JSON.parse(text)
-  }
-  catch (e) {
-    console.error(e)
-    return undefined
-  }
+	try {
+		return JSON.parse(text)
+	} catch (e) {
+		console.error(e)
+		return undefined
+	}
 }
 
 function getWholeTextRange(textEditor: TextEditor) {
-  const firstLine = textEditor.document.lineAt(0)
-  const lastLine = textEditor.document.lineAt(textEditor.document.lineCount - 1)
-  return new vscode.Range(0,
-    firstLine.range.start.character,
-    textEditor.document.lineCount - 1,
-    lastLine.range.end.character)
+	const firstLine = textEditor.document.lineAt(0)
+	const lastLine = textEditor.document.lineAt(textEditor.document.lineCount - 1)
+	return new Range(
+		0,
+		firstLine.range.start.character,
+		textEditor.document.lineCount - 1,
+		lastLine.range.end.character
+	)
 }
